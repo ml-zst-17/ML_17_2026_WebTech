c92b40d8b0f8741ec73ae892d9b0d7d0977512fa
ml-zst-17 <michele.laba@zst.tarnow.pl>
2026-09-10 14:18:12 +0200

    Pt. 16

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import './App.css'
 
 function App() {
-  return <></>
+  const app = {
+    name: "WebTech",
+    version: "1.0",
+    author: "Michele Łaba",
+    technologiesCount: 3,
+  };
+
+  return (
+    <div>
+      <h1>{app.name}</h1>
+      <p>Wersja: {app.version}</p>
+      <p>Autor: {app.author}</p>
+      <p>
+        Liczba technologii: {app.technologiesCount}
+      </p>
+    </div>
+  );
 }
 
 export default App
