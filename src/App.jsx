0baa179d17940476b6aa602d17b2cb482913c7c9
ml-zst-17 <michele.laba@zst.tarnow.pl>
2026-09-17 11:54:01 +0200

    Nowy pusty plik

--- src/App.jsx
+++ src/App.jsx
@@ -3,58 +3,8 @@
 import './App.css'
 
 function App() {
-  const app = {
-    name: "WebTech",
-    version: "1.0",
-    author: "Michele Łaba",
-    technologiesCount: 3,
-  };
-
-  const technology = {
-    name: "React",
-    category: "Frontend",
-    hours: 30,
-    active: true,
-  };
-
-  const student = {
-    name: "Michele",
-    surname: "Łaba",
-    className: "4P",
-    specialization: "technik programista",
-  };
-
-  const course = {
-    name: "Zaawansowane Aplikacje Webowe",
-    teacher: "Rafał Taraszka",
-    hours: 160,
-    completed: false,
-  }
-
   return (
-    <div>
-      <h1>{app.name}</h1>
-      <p>Wersja: {app.version}</p>
-      <p>Autor: {app.author}</p>
-      <p>
-        Liczba technologii: {app.technologiesCount}
-      </p>
-
-      <h2>Technologia użyta: {technology.name}</h2>
-      <p>Kategoria: {technology.category}</p>
-      <p>Liczba godzin: {technology.hours}</p>
-
-      <h2>Uczeń: {student.name} {student.surname}</h2>
-      <p>Klasa: {student.className}</p>
-      <p>Kierunek: {student.specialization}</p>
-
-      <section>
-        <h2>Kurs: {course.name}</h2>
-        <p>Nauczyciel: {course.teacher}</p>
-        <p>Ile trwa: {Math.ceil(course.hours / 24)}d</p>
-        <p>Skończony: <span className={!course.completed ? 'big-red' : ''}>{course.completed.toString()}</span></p>
-      </section>
-    </div>
+    <></>
   );
 }
 
